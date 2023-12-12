3a557d05fa067ea8962e6c25952ad3ebdaffd7e8
Kalin Hristov <kalin0169@gmail.com>
2023-12-12 13:08:01 +0200

    Adding State in the App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
+import { useState } from 'react';
+
 import Header from './components/Header.tsx';
 import goalsImg from './assets/goals.jpg';
 import PersonalGoal from './components/PersonalGoal.tsx';
 
+interface PersonalGoal {
+  title: string;
+  description: string;
+  id: number;
+}
+
 const App = () => {
+  const [goals, setGoals] = useState<PersonalGoal[]>([]);
+
+  function addGoalHandler() {
+    console.log('button is clicked');
+
+    setGoals((prevGoals) => {
+      const newGoal: PersonalGoal = {
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+        id: Math.random(),
+      };
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Personal Goals</h1>
       </Header>
-      <PersonalGoal title='Learn React + TS'>
-        <p>Learn it from the ground up</p>
-      </PersonalGoal>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <PersonalGoal title={goal.title} description={goal.description} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
