479a059d25ecb4a80c9b7517fbf89e5759666ec9
Kalin Hristov <kalin0169@gmail.com>
2023-12-12 10:22:03 +0200

    Create PersonalGoal.tsx

---
+++ src/components/PersonalGoal.tsx
@@ -0,0 +1,19 @@
+
+const PersonalGoal = () => {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  )
+}
+
+export default PersonalGoal
+
+
+
+
+
