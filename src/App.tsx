33f31ead87994f723c72d3664b58efd0c1807306
Kalin Hristov <kalin0169@gmail.com>
2023-12-12 11:38:42 +0200

    Crating Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Header from './components/Header.tsx';
+import goalsImg from './assets/goals.jpg';
 import PersonalGoal from './components/PersonalGoal.tsx';
 
 const App = () => {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Personal Goals</h1>
+      </Header>
       <PersonalGoal title='Learn React + TS'>
         <p>Learn it from the ground up</p>
       </PersonalGoal>
