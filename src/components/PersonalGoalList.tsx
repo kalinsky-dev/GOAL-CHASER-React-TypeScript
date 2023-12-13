990f7efda44403fd3bd58de134ee7f12bbc33bb7
Kalin Hristov <kalin0169@gmail.com>
2023-12-13 11:01:55 +0200

    Update PersonalGoalList with onDeleteGoal functionality

--- src/components/PersonalGoalList.tsx
+++ src/components/PersonalGoalList.tsx
@@ -3,14 +3,20 @@ import { IPersonalGoal } from '../App.tsx';
 
 interface IPersonalGoalListProps {
   goals: IPersonalGoal[];
+  onDeleteGoal: (goalId: number) => void;
 }
 
-const PersonalGoalList = ({ goals }: IPersonalGoalListProps) => {
+const PersonalGoalList = ({ goals, onDeleteGoal }: IPersonalGoalListProps) => {
   return (
     <ul>
       {goals.map((goal) => (
         <li key={goal.id}>
-          <PersonalGoal title={goal.title} description={goal.description} />
+          <PersonalGoal
+            title={goal.title}
+            description={goal.description}
+            id={goal.id}
+            onDelete={onDeleteGoal}
+          />
         </li>
       ))}
     </ul>
