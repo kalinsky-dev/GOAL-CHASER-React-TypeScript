c86769d09dfe9b8c183c46cf86def0a57cfd0282
Kalin Hristov <kalin0169@gmail.com>
2023-12-13 11:02:16 +0200

    onDelete Goal Works!

--- src/components/PersonalGoal.tsx
+++ src/components/PersonalGoal.tsx
@@ -1,17 +1,23 @@
-
 interface IPersonalGoalProps {
   title: string;
   description: string;
+  id: number;
+  onDelete: (goalId: number) => void;
 }
 
-const PersonalGoal = ({ title, description }: IPersonalGoalProps) => {
+const PersonalGoal = ({
+  title,
+  description,
+  id,
+  onDelete,
+}: IPersonalGoalProps) => {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   );
 };
