4589fd4c647f1c0cd633ec209e4d5f052aa70432
Kalin Hristov <kalin0169@gmail.com>
2023-12-14 10:12:05 +0200

    Import the InfoBox Component in  PersonalGoalList Componet.
    Info Warnings Work!

--- src/components/PersonalGoalList.tsx
+++ src/components/PersonalGoalList.tsx
@@ -1,5 +1,8 @@
+import { type IPersonalGoal } from '../App.tsx';
+import { type ReactNode } from 'react';
+
 import PersonalGoal from './PersonalGoal.tsx';
-import { IPersonalGoal } from '../App.tsx';
+import InfoBox from './InfoBox.tsx';
 
 interface IPersonalGoalListProps {
   goals: IPersonalGoal[];
@@ -7,19 +10,35 @@ interface IPersonalGoalListProps {
 }
 
 const PersonalGoalList = ({ goals, onDeleteGoal }: IPersonalGoalListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>You have no goals yet. Why not add one?</InfoBox>
+    );
+  }
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning'>
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <PersonalGoal
-            title={goal.title}
-            description={goal.description}
-            id={goal.id}
-            onDelete={onDeleteGoal}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <PersonalGoal
+              title={goal.title}
+              description={goal.description}
+              id={goal.id}
+              onDelete={onDeleteGoal}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
